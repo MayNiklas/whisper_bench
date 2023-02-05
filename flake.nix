faa0f8401bc671af75fb79e087f39f5ee1462987
MayNiklas <info@niklas-steffen.de>
2023-02-05 11:23:59 +0100

    add flake.nix

---
+++ flake.nix
@@ -0,0 +1,32 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = nixpkgs.legacyPackages.${system};
+      in
+      rec {
+        # Use nixpkgs-fmt for `nix fmt'
+        formatter = pkgs.nixpkgs-fmt;
+        defaultPackage = packages.whisper_bench;
+
+        packages = flake-utils.lib.flattenTree rec {
+
+          whisper_bench = with pkgs.python310Packages;
+            buildPythonPackage rec {
+              pname = "whisper_bench";
+              version = "1.0.0";
+              src = self;
+              propagatedBuildInputs = [
+                openai-whisper
+                torch
+              ];
+              doCheck = false;
+            };
+
+        };
+      });
+}
